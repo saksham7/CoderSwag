ec1c6d6a90967670bf2f086d38c722a5e2ebb641
Saksham <sakshamsrivastava74@gmail.com>
2018-07-20 00:11:24 +0530

    ListView Array Adapter Added.

--- app/src/main/java/com/example/sakshamspc/coderswag/Model/Category.kt
+++ app/src/main/java/com/example/sakshamspc/coderswag/Model/Category.kt
@@ -3,4 +3,8 @@ package com.example.sakshamspc.coderswag.Model
 /**
  * Created by Saksham's PC on 7/19/2018.
  */
-class Category (val title:String,val image:String)
+class Category (val title:String,val image:String){
+    override fun toString(): String {
+        return title
+    }
+}
